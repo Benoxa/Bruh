04942b4a7033fc9781cde76f659e48ee44e9430b
Benoxa <80475066+Benoxa@users.noreply.github.com>
2022-03-26 18:49:45 +0300

    Update and rename ModChecks to ModChecks.lua

---
+++ ModChecks.lua
@@ -0,0 +1,25 @@
+MODS = {
+	[1787184971] = true -- owner
+}
+
+ADMINS = {
+	[1787184971] = true
+}
+
+function initiateNames()
+	game.Players.LocalPlayer.Character.LowerTorso:FindFirstChild('OriginalSize'):Destroy()
+	for _,v in pairs(game:GetService('Players'):GetPlayers()) do
+		if v.Character then
+			if ADMINS[v.UserId] then
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑] ' .. v.DisplayName)
+			elseif MODS[v.UserId] then
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐] ' .. v.DisplayName)
+			elseif not v.Character.LowerTorso:FindFirstChild('OriginalSize') then
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😎] ' .. v.DisplayName)
+			end
+		end
+	end
+end
+
+local successful, errored = pcall(initiateNames)
+return MODS, ADMINS
